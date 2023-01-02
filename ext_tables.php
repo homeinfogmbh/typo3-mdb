444e6f881ab8909793af226c795b89f6b80b094b
Richard Neumann <r.neumann@homeinfo.de>
2023-01-02 11:41:32 +0100

    Add mdb controller

--- ext_tables.php
+++ ext_tables.php
@@ -3,3 +3,20 @@ defined('TYPO3_MODE') || die();
 
 // allow table entries to be added to normal pages
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_mdb_domain_model_customer');
+
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+    'mdb',
+    'mdb',
+    '',
+    '',
+    [
+        \Homeinfo\mdb\Controller\DebugController::class => 'index',
+    ],
+    [
+        'access' => 'user',
+        'labels' => 'LLL:EXT:mdb/Resources/Private/Language/locallang_be.xlf:backend.customers.label',
+        'inheritNavigationComponentFromMainModule' => false,
+        'standalone' => true,
+    ]
+);
