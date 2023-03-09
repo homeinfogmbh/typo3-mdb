13e5e77e26c5fd7ca5828e8d4cfa9338d7793495
Richard Neumann <r.neumann@homeinfo.de>
2023-03-09 11:47:40 +0100

    Remove debugging

--- Classes/Domain/Model/Address.php
+++ Classes/Domain/Model/Address.php
@@ -25,7 +25,6 @@ final class Address
             if (str_starts_with($key, $prefix))
                 $addressFields[substr($key, strlen($prefix))] = $value;
 
-        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($addressFields, "Extracted address: ");
         return Self::fromArray($addressFields);
     }
 
